aebe5774cae6e035ac8e9dbaee42d7f4a7cd0bff
Silos <gagamelius.idk@gmail.com>
2026-06-17 18:01:36 +0200

    Adjust procedural generation parameters and implement camera zoom functionality for game board

--- presentation/board/game_board.gd
+++ presentation/board/game_board.gd
@@ -2,6 +2,8 @@ extends Node2D
 ## Zeigt das Spielbrett an und reagiert auf Core-Events.
 ## Liest aus GameState, schickt Spieler-Eingaben als Commands an den Core.
 
+@onready var camera = $Camera2D
+
 
 @onready var hex_map = $HexTileMap
 
@@ -26,7 +28,24 @@ func build_from_state() -> void:
 		hex_map.set_terrain(coord, tile.terrain)
 		if tile.number_token > 0:
 			_add_token(coord, tile.number_token)
-		
+	zoom_factor()
+	
+func zoom_factor() -> void:
+	var min := Vector2(INF, INF)
+	var max := Vector2(-INF, -INF)
+	var half := Vector2(hex_map.TILE_SIZE) / 2
+	
+	for coord in GameState.tiles:
+		var center : Vector2 = hex_map.hex_to_world(coord)
+		min = min.min(center - half)
+		max = max.max(center + half)
+	
+	var bs := (max - min)* 1.2
+	var view := get_viewport_rect().size
+	var zoomfactor : float = min(view.x/bs.x,view.y/bs.y)
+	camera.zoom = Vector2(zoomfactor,zoomfactor)
+	camera.position = (min + max) / 2
+	
 
 func _add_token(coord: Vector2i, value: int) -> void:
 	var label := Label.new()
